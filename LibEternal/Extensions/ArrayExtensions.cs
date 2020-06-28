636068f68e3187f291264c390ea4f2d701e94b30
EternalClickbait <48875125+EternalClickbait@users.noreply.github.com>
2020-06-28 17:20:02 +1000

    Create ArrayExtensions.cs

---
+++ LibEternal/Extensions/ArrayExtensions.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+
+namespace LibEternal.Extensions
+{
+	[PublicAPI]
+	public static class ArrayExtensions
+	{
+		public static bool ContentsEqual<T>(this T[] array1, T[] array2)
+		{
+			if (ReferenceEquals(array1, array2))
+				return true;
+
+			if (array1 == null || array2 == null)
+				return false;
+
+			if (array1.Length != array2.Length)
+				return false;
+
+			EqualityComparer<T> comparer = EqualityComparer<T>.Default;
+			for (int i = 0; i < array1.Length; i++)
+			{
+				if (!comparer.Equals(array1[i], array2[i])) return false;
+			}
+			return true;
+		}
+	}
+}
