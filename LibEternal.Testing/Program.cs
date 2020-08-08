910266dc09c03ca18ce5919b979ac7d273d4e2a2
EternalClickbait <48875125+EternalClickbait@users.noreply.github.com>
2020-08-08 16:46:20 +1000

    Remove not needed serilog packages

--- LibEternal.Testing/Program.cs
+++ LibEternal.Testing/Program.cs
@@ -1,37 +1,17 @@
 ﻿using Serilog;
-using Serilog.Context;
-using System;
 
 namespace LibEternal.Testing
 {
-	class Program
+	internal static class Program
 	{
-		static void Main(string[] args)
+		private static void Main()
 		{
 			Log.Logger = new LoggerConfiguration()
-				.WriteTo.Console(outputTemplate:"[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
-				.Enrich.WithProcessId()
-				.Enrich.WithProcessName()
-				.Enrich.FromLogContext()
+				.WriteTo.Console(outputTemplate:"[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}{NewLine}")
 				.CreateLogger();
 			
-			Log.Information("No contextual properties");
-
-			using (LogContext.PushProperty("A", 1))
-			{
-				Log.Information("Carries property A = 1");
-
-				using (LogContext.PushProperty("A", 2))
-					using (LogContext.PushProperty("B", 1))
-					{
-						Log.Information("Carries A = 2 and B = 1");
-					}
-
-				Log.Information("Carries property A = 1, again");
-			}
 			
-			Log.Information("TEST");
-			
+
 			Log.CloseAndFlush();
 		}
 	}
