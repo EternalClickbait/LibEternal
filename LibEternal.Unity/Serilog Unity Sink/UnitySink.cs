393603237cecb73130a28116d9bbc45119230568
EternalClickbait <48875125+EternalClickbait@users.noreply.github.com>
2020-08-09 13:28:32 +1000

    Create a Unity sink for Serilog

---
+++ LibEternal.Unity/Serilog Unity Sink/UnitySink.cs
@@ -0,0 +1,58 @@
+﻿using JetBrains.Annotations;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Formatting.Display;
+using System;
+using System.IO;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace Serilog.Sinks.Unity
+{
+	/// <summary>
+	/// An <see cref="ILogEventSink"/> for Unity
+	/// </summary>
+	[PublicAPI]
+	public sealed class UnitySink : ILogEventSink
+	{
+		private readonly ITextFormatter formatter;
+
+		/// <summary>
+		/// Constructs a new <see cref="UnitySink"/>, using the optional <see cref="IFormatProvider"/>
+		/// </summary>
+		/// <param name="formatProvider"></param>
+		public UnitySink([NotNull] string template, IFormatProvider formatProvider)
+		{
+			formatter = new MessageTemplateTextFormatter(template, formatProvider);
+		}
+
+		/// <inheritdoc />
+		public void Emit(LogEvent logEvent)
+		{
+			using (StringWriter writer = new StringWriter())
+			{
+				formatter.Format(logEvent, writer);
+				string message = writer.ToString();
+				
+				switch (logEvent.Level)
+				{
+					case LogEventLevel.Fatal:
+					case LogEventLevel.Error:
+						Debug.LogError(message);
+						break;
+					case LogEventLevel.Warning:
+						Debug.LogWarning(message);
+						break;
+					case LogEventLevel.Information:
+					case LogEventLevel.Debug:
+					case LogEventLevel.Verbose:
+						Debug.Log(message);
+						break;
+					default:
+						throw new ArgumentOutOfRangeException(nameof(logEvent));
+				}
+			}
+		}
+	}
+}
---
+++ LibEternal.Unity/Serilog Unity Sink/UnitySinkExtensions.cs
@@ -0,0 +1,24 @@
+﻿using LibEternal.JetBrains.Annotations;
+using Serilog.Configuration;
+using System;
+
+// ReSharper disable once CheckNamespace
+namespace Serilog.Sinks.Unity
+{
+	[PublicAPI]
+#pragma warning disable 1591
+	public static class UnitySinkExtensions
+#pragma warning restore 1591
+	{
+		/// <summary>
+		///     Writes log events to Unity's <see cref="UnityEngine.Debug" /> class;
+		/// </summary>
+		/// <param name="loggerSinkConfiguration">Logger sink configuration</param>
+		/// <param name="formatProvider">An <see cref="IFormatProvider" /> used to render the messages</param>
+		/// <returns>A <see cref="LoggerConfiguration" /> that can be method-chained</returns>
+		public static LoggerConfiguration Unity3D(this LoggerSinkConfiguration loggerSinkConfiguration, string template, IFormatProvider formatProvider = null)
+		{
+			return loggerSinkConfiguration.Sink(new UnitySink(template, formatProvider));
+		}
+	}
+}
