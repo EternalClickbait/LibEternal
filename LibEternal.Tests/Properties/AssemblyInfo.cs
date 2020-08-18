084817483a9824c34190598eda60ce41f74a4485
EternalClickbait <48875125+EternalClickbait@users.noreply.github.com>
2020-08-18 10:27:05 +1000

    Switch to NUnit, revert to netstandard 2.1

--- LibEternal.Tests/Properties/AssemblyInfo.cs
+++ LibEternal.Tests/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("609C6E8B-0496-4D49-815E-44C15515E860")]
+[assembly: Guid("A626CDB9-C72E-4C62-BA59-145EFEE859D7")]
 
 // Version information for an assembly consists of the following four values:
 //
