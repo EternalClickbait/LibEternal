16802bfbe8556cd5b416f8ad2a31e2faf269dabe
EternalClickbait <48875125+EternalClickbait@users.noreply.github.com>
2020-12-12 18:52:04 +1000

    Update AssemblyExtensions.cs
    Add GetInheritedTypes extensions

--- LibEternal/Extensions/AssemblyExtensions.cs
+++ LibEternal/Extensions/AssemblyExtensions.cs
@@ -31,5 +31,30 @@ namespace LibEternal.Extensions
 				return e.Types.Where(t => t != null);
 			}
 		}
+
+		public static IEnumerable<Type> GetInheritedTypes(this Assembly assembly, Type baseType, bool includeAbstract = false, bool includeInterfaces = false)
+		{
+			return assembly.GetTypesSafe()
+				//
+				.Where(type => type.IsSubclassOf(baseType))
+				/*
+				 * Here's a little truth table I created for what I want this function to select
+				 * type.IsAbstract 	|	includeAbstract		|	Include(Out)
+				 * N				|	N					|	Y
+				 * N				|	Y					|	Y
+				 * Y				|	N					|	N
+				 * Y				|	Y					|	Y
+				 *
+				 * We only skip the type when it is abstract but we don't want to include them (type.IsAbstract && !includeAbstract).
+				 * So we can just negate it to get the types we want to keep
+				 * `!(type.IsAbstract && !includeAbstract)` or `!type.IsAbstract || includeAbstract`
+				 */
+				.Where(type => !type.IsAbstract || includeAbstract)
+				//Also do the same for interfaces
+				.Where(type => !type.IsInterface || includeInterfaces);
+
+
+
+		}
 	}
 }
